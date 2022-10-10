2a8ad05273d61926a55fdbd20c5c46da620d24ae
Tshepiso Mahoko <2352695@students.wits.ac.za>
2022-10-10 20:33:08 +0200

    loadingspinner

--- src/Screens/ShowDocument/ShowDocument.js
+++ src/Screens/ShowDocument/ShowDocument.js
@@ -5,31 +5,43 @@ import { ref,listAll, uploadBytesResumable, getDownloadURL } from "firebase/stor
 import './ShowDocument.css'
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
-
+import LoadingSpinner from "../../Components/LoadingSpinner/LoadingSpinner"
 function ShowDocument() {
     const { state } = useLocation();
     const {file} = state;      //state from previous page
     const fileRef = ref(storage, "files/");
     const [Url, setUrl] = useState('');            // url for file which is about to be displayed
-
-   
+    const [isLoading, setIsLoading] = useState(true); 
   useEffect(() => {
+    //setIsLoading(false)
     listAll(fileRef).then((response) => {
       response.items.forEach((item) => {
         getDownloadURL(item).then((url) => {
           if(url.includes(file.file)){setUrl(url);}  
         });
+
       });
+        setIsLoading(false); 
+        //setTimeout(() => renderPDF, 3000);
     });
 
   }, []);
 
+  const renderPDF = (
+    <div >
+      <object width="100%" height="600px" data={Url} type="application/pdf" >   </object>
+    </div>
+  );
+
     return (
         <div>
-            <div class="header"><Header /></div>
-            <div className='filename'>{file.filename}</div>
-            <object width="100%" height="600px" data={Url} type="application/pdf">   </object>
-            <div class="footer"><Footer /></div>    
+          <div class="header"><Header /></div>
+          <div className='filename'>{file.filename}</div>
+          {isLoading && <div> <LoadingSpinner /> </div>}
+          {!isLoading && <div>
+            <object width="100%" height="600px" data={Url} type="application/pdf" >   </object> </div>} 
+     
+          <div class="footer"><Footer /></div> 
         </div>
     )
 }
